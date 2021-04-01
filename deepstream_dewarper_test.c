b6eba0476599719c35d65f27ee17390418c30937
mdesta <mdesta@nvidia.com>
2021-04-01 00:20:07 +0000

    c file structure

--- deepstream_dewarper_test.c
+++ deepstream_dewarper_test.c
@@ -27,6 +27,7 @@
 #include <math.h>
 #include <string.h>
 #include <sys/time.h>
+#include <unistd.h>
 
 #include "gstnvdsmeta.h"
 #ifndef PLATFORM_TEGRA
@@ -730,9 +731,9 @@ main (int argc, char *argv[])
     g_printerr ("Failed to create nvdewarper element. Exiting.\n");
     return -1;
   }
-
+  
   g_object_set (G_OBJECT (nvinfer), 
-    "config-file-path", "./inference_files/config_infer_primary_peoplenet.txt",
+    "config-file-path", "inference_files/config_infer_primary_peoplenet.txt", 
     NULL);
 
   /* Use nvtiler to composite the batched frames into a 2D tiled array based
@@ -804,7 +805,9 @@ main (int argc, char *argv[])
       "width", TILED_OUTPUT_WIDTH, "height", TILED_OUTPUT_HEIGHT, NULL);
 
   char name[300];
-  snprintf(name, 300, "/tracker_files/dstest_tracker_config.txt");
+  snprintf(name, 300, "tracker_files/dstest_tracker_config.txt");
+  
+  
   if(!set_tracker_properties(tracker, name)){
 	  g_printerr("Failed to set tracker properties. Exiting.\n");
 	  return -1; 
